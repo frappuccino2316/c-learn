2b2f73b0c006aab33426962927dd2b4f9185843c
frappuccino2316 <frappuccino2316@gmail.com>
2024-02-13 22:42:33 +0900

    ポインタと参照

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <string>
 
 int main() {
-    int a;
-    std::cout << "数値を入力してください:";
-    std::cin >> a;
-    int b = a * 2;
-    std::cout << a << "を2倍した数は、" << b << "です。" << std::endl;
+    int a = 13;
+    int* b = &a;
+    int c = a;
+    int* d = &c;
+    int& e = a;
+    int* f = &e;
+    std::cout << a << ", " << b << ", " << c << ", " << d << ", " << e << ", " << f << std::endl;
 
     return 0;
 }
