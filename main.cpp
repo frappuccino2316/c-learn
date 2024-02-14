e697f914e42205a45e521af2163b4dc409951d3a
frappuccino2316 <frappuccino2316@gmail.com>
2024-02-15 00:16:47 +0900

    メモリ操作をお試し

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 
-int Add(int a, int b);
-
 int main() {
-    int (*func)(int, int) = Add;
+    int* p1 = new int(100);
+    int* p2 = new int[5];
 
-    int a = func(14, 56);
+    *p1 = 200;
 
-    std::cout << a << std::endl;
+    p2[0] = 2;
+    p2[1] = 42;
+    p2[2] = 53;
+    p2[3] = 172;
+    p2[4] = 19273;
 
-    return 0;
-}
+    std::cout << *p1 << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        std::cout << p2[i] << std::endl;
+    }
+    std::cout << p1 << ", " << p2 << std::endl;
 
-int Add(int a, int b) {
-    return a + b;
+    delete p1;
+    delete[] p2;
 }
